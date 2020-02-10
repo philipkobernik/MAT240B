32c48adbaf1a5386e5309156736933ae3ab88db9
Karl Yerkes <karl.yerkes@gmail.com>
2020-02-09 18:41:17 -0800

    reverse examples

---
+++ unix/reverse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// reverse a sound clip
+//
+// - takes lines of floats
+// - gives lines of floats
+//
+//
+//
+
+int main() {
+  // read in a sequence of lines where each line is a human readable floating
+  // point number.
+  //
+  string line;
+  getline(cin, line);
+  vector<float> sample;
+  while (line != "") {
+    float f = stof(line);
+    getline(cin, line);
+    sample.push_back(f);
+  }
+
+  // write a sequence of lines where each line is a human readable floating
+  // point number.
+  //
+  while (sample.size() > 0) {
+    printf("%f\n", sample.back());
+    sample.pop_back();
+  }
+}
